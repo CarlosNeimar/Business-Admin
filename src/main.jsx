53976de65914f82b592adc6476602c9e346e4451
Carlos Henrique Neimar <carlos.areas@sga.pucminas.br>
2023-12-20 22:58:12 -0300

    First Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Router
+
+import{ createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
---
+++ src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Está será a home</h1>
+        </div>
+    )
+}
+
+export default Home
