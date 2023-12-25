64be5bf6bb37cd9126480823a841db2ad07645ee
Carlos Henrique Neimar <carlos.areas@sga.pucminas.br>
2023-12-25 16:48:46 -0300

    Update Settings

--- src/layout/Config.jsx
+++ src/layout/Config.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { IoCloseOutline } from "react-icons/io5";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+import { FaQuestion } from "react-icons/fa";
+
+
 
 
 import { Temas } from '../components/temas';
 
 import './Config.css';
+
 
 const Config = ({ isVisible, toggleVisibility, isdark, setisdark}) => {
   const componentClass = isVisible ? 'Cardconfig' : 'hidden';
@@ -12,8 +19,10 @@ const Config = ({ isVisible, toggleVisibility, isdark, setisdark}) => {
     toggleVisibility();
   }; 
 
+  
+
   return (
-    <div>
+    <div data-tema={isdark ? "dark" : "light"}>
       <div className={componentClass}>
         <div className="cardcontainer">
           <div className='configtitle'>
@@ -27,9 +36,17 @@ const Config = ({ isVisible, toggleVisibility, isdark, setisdark}) => {
                 <Temas ischecked={isdark} heandlechange={() => setisdark(!isdark)} />
               </div>
               <div className="links">
-                <a href="">Criador</a>
-                <a href="">Feedback</a>
-                <a href="">Sobre</a>
+                <ul>
+                <li className='lipers'>
+                <a href=""><FaArrowUpRightFromSquare /> GitHub <FaGithub /></a>
+                </li>
+                <li className='lipers'>
+                <a href=""><FaArrowUpRightFromSquare /> Linkedin <CiLinkedin /></a>
+                </li>
+                <li className='lipers'>
+                <a href=""><FaArrowUpRightFromSquare /> Sobre <FaQuestion /></a>
+                </li>
+                </ul>
           </div>
         </div>
       </div>
