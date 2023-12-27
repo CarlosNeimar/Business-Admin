5566fb7ac470ad4fe893edc5bc2eac06207fd757
Carlos Henrique Neimar <carlos.areas@sga.pucminas.br>
2023-12-27 02:44:08 -0300

    First step create business

--- src/routes/Novonegocio-sites/Novotype.jsx
+++ src/routes/Novonegocio-sites/Novotype.jsx
@@ -1,13 +1,44 @@
 import "./Novotype.css"
+import { LiaBusinessTimeSolid } from "react-icons/lia";
+import { MdOutlineAddBusiness } from "react-icons/md";
+
 
 const Novotype = ({isdark, setisdark}) => {
 
     return(
         <div data-tema={isdark ? "dark" : "light"}>
         <div className="novonegocio">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis provident excepturi qui, odio ducimus laudantium nihil totam nulla fugiat voluptatum suscipit quaerat dolores natus earum sint est optio neque blanditiis. A repudiandae dolores iste vero nesciunt laborum mollitia quos? Hic, doloribus quam nemo adipisci nostrum beatae molestias reprehenderit illo ipsa.
-
-
+        <div className="contentnew">
+        <div className="ops">
+        <div className="titlenew">
+            <h1>O seu negocio já existe ?</h1>
+        </div>
+        <div className="artnegocios">
+        <div className="cardnegocios">
+            <div className="titlecard">
+            <div className="cabecalhocard">
+            <h2>Sim!</h2>
+            <h3>Quero apenas gerenciá-lo</h3>
+            <div className="iconscreate">
+            <LiaBusinessTimeSolid className="iconetype"/>
+            </div>
+            </div>
+            </div>
+        </div>
+        <div className="cardnegocios">
+        <div className="titlecard">
+            <div className="cabecalhocard">
+            <h2>Não!</h2>
+            <h3>Quero criá-lo do zero</h3>
+            <div className="iconscreate">
+            <MdOutlineAddBusiness className="iconetype"/>
+            </div>
+            </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
         </div>
         </div>
     )
