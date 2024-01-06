fbf7d82e57d64ad63859eecb0b6fb33bc5f3c28b
Carlos Henrique Neimar <carlos.areas@sga.pucminas.br>
2024-01-06 15:44:44 -0300

    Update Home

---
+++ src/routes/Home-sections/Sobre.jsx
@@ -0,0 +1,19 @@
+import "./Homes.css"
+
+const Sobre = () => {
+
+    return(
+        <section className="Sobrehome">
+            <div className="cardsobre">
+            <div className="conteudosobre">
+            <h1>Sobre</h1>
+            <p>Este site é uma plataforma robusta de administração empresarial, projetada para simplificar e otimizar a gestão de empresas e negócios. Oferece ferramentas abrangentes para monitorar e controlar diversos aspectos operacionais, como finanças, recursos humanos, logística e produção. Com uma interface intuitiva, permite aos gestores acessar informações cruciais, tomar decisões informadas e automatizar processos, resultando em eficiência aprimorada. Além disso, fornece recursos de análise de dados, relatórios personalizáveis e integração com outras aplicações, tornando-se uma solução completa para administradores que buscam melhorar a produtividade e a eficácia em suas operações empresariais.</p>
+            </div>
+            </div>
+
+        </section>
+    )
+
+}
+
+export default Sobre;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Start from "./Home-sections/Start";
 import Irdash from "./Home-sections/Irdash";
+import Sobre from "./Home-sections/Sobre";
 
 const Home = () => {
     return (
         <div>
             <Start/>
+            <Sobre />
             <Irdash />
         </div>
     )
