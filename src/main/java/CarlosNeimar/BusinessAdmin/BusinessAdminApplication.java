1207e11f3e37589f99b33f78423c319836108e20
Carlos Henrique Neimar <carlos.areas@sga.pucminas.br>
2024-01-11 20:52:36 -0300

    New Spring install

---
+++ src/main/java/CarlosNeimar/BusinessAdmin/BusinessAdminApplication.java
@@ -0,0 +1,13 @@
+package CarlosNeimar.BusinessAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessAdminApplication.class, args);
+	}
+
+}
